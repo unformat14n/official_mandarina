a9c933ca2a024b851e81c6ce52b5918c01c61c44
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2025-04-14 15:41:36 -0600

    Add theme context to save dark/light theme

---
+++ mandarina/contexts/ThemeContext.js
@@ -0,0 +1,54 @@
+// src/context/ThemeContext.js
+import React, { createContext, useState, useEffect, useContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Appearance } from "react-native";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState("light")
+
+    // Load saved theme preference
+    useEffect(() => {
+        const loadThemePreference = async () => {
+            try {
+                const savedTheme = await AsyncStorage.getItem(
+                    "theme"
+                );
+                if (savedTheme !== null) {
+                    setTheme(savedTheme);
+                } else {
+                    // If no preference saved, use system preference
+                    const systemColorScheme = Appearance.getColorScheme();
+                    setTheme(systemColorScheme);
+                }
+            } catch (error) {
+                console.error("Error loading theme preference:", error);
+            }
+        };
+
+        loadThemePreference();
+    }, []);
+
+    // Toggle between dark and light theme
+    const toggleTheme = async () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        try {
+            await AsyncStorage.setItem(
+                "theme",
+                theme
+            );
+        } catch (error) {
+            console.error("Error saving theme preference:", error);
+        }
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
